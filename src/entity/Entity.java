899c592140cee03fabd98cffc3c4ff9b26f0b68e
DNhan3 <96424205+DNhan3@users.noreply.github.com>
2025-05-16 22:10:27 +0700

    animation

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -11,7 +11,8 @@ public class Entity {
 
     public int speedX = 0;
     public int speedY = 0;
-    public String direction = "";
+    public String direction = "right";
+    public String oldAction = "idle";
     public boolean grounded = false;
     public boolean[] collisionOn = new boolean[5]; // 1 = up, 2 = down, 3 = left, 4 = right check direction of collision
     public int[] collisionTile = new int[5]; // 1 = up, 2 = down, 3 = left, 4 = right check tile number of collision
